da340c7f064143734b4a17ceeae70222ae4e3667
guhanganesan <phpguhan@gmail.com>
2023-07-11 19:20:13 +0530

    check event type

--- React/my-react-app/src/pages/Blogs.js
+++ React/my-react-app/src/pages/Blogs.js
@@ -8,13 +8,18 @@ function Blog(props){
         console.log("blog data: ", blog_data)
     }
 
-    return  <>
+    const find_event = (x,y)=>{
+        console.log("event type: ", y.type)
+        console.log("msg: ", x)
+    }
 
+    return  <>
             {/* <h1>I am {props.blog_name} </h1> */}
             <button onClick={shoot}>click</button>
             <br></br>
             <button onClick={()=> display(props.blog_data) }>click data</button>
-
+            <br></br>
+            <button onClick={(event)=> find_event("Hello", event) }>check event</button>
             </>
 }
 
