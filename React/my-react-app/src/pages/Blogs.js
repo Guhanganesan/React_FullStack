93ad894e2142195565da5354c4c3c66e5f15934d
guhanganesan <phpguhan@gmail.com>
2023-07-11 15:42:51 +0530

    event example-1

--- React/my-react-app/src/pages/Blogs.js
+++ React/my-react-app/src/pages/Blogs.js
@@ -1,6 +1,13 @@
 function Blog(props){
+
+    const shoot = () => {
+        alert("Great Shot!");
+      }
+
     return  <>
+
             <h1>I am {props.blog_name} </h1>
+            <button onClick={shoot}>Take the shot!</button>
             </>
 }
 
